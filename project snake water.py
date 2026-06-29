f8145a2f0605c76872bc84196d70d938361a264e
Aman mathur <amanmathur.45678@gmail.com>
2026-06-29 12:19:37 +0530

    snake water gaame

---
+++ project snake water.py
@@ -0,0 +1,41 @@
+import random
+
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+
+computer =random.choice([-1,0,1])
+youstr=input("enter your choice: ")
+youDict={"s": 1,"w": -1,"g": 0}
+reverseDict={1: "snake",-1:"water",0:"Gun"}
+you=youDict[youstr]
+
+print(f"you choose {reverseDict[you]}\ncomputer choose{reverseDict[computer]}")
+
+if(computer==you):
+    print("it's a draw")
+
+else:
+    if(computer==-1 and you==1):
+        print("you win!")
+
+    elif(computer==-1 and you==0):
+        print("you lose!")
+        
+    elif(computer==1 and you==-1):
+        print("you lose!")
+
+    elif(computer==1 and you==0):
+        print("you Win!")
+
+
+    elif(computer==0 and you==-1):
+        print("you Win!")
+
+    elif(computer==0 and you==1):
+        print("you lose!")
+
+    else:
+        print("something went wrong!")    
